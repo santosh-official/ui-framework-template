53677f8bd76e62ef35d1ce641402eed33e74076c
Santosh Kumar Hota <santosh.babul1990@gmail.com>
2022-09-13 14:24:50 +0530

    Add files via upload

--- projects/cargill-alert/src/lib/cargill-alert.component.ts
+++ projects/cargill-alert/src/lib/cargill-alert.component.ts
@@ -1,1 +1,26 @@
+import { Component, Input, OnInit } from '@angular/core';
 
+@Component({
+  selector: 'cargill-alert',
+  template: `
+    <div class="alert alert-dismissible fade show" [style.backgroundColor]='bgColor' role="alert">
+      <p>{{message}}</p>
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+  `,
+  styles: [`
+    .alert { padding-bottom: 0 }
+  `]
+})
+export class CargillAlertComponent implements OnInit {
+
+  @Input() message: any;
+  @Input() bgColor: any = '#BFE0CE'
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+}
---
+++ projects/cargill-alert/src/lib/cargill-alert.module.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CargillAlertComponent } from './cargill-alert.component';
+
+@NgModule({
+  declarations: [
+    CargillAlertComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    CargillAlertComponent
+  ]
+})
+export class CargillAlertModule { }
