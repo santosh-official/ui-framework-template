968e6763cda109625487d2752881ab145594e59a
Santosh Kumar Hota <santosh.babul1990@gmail.com>
2022-09-13 14:29:11 +0530

    Add files via upload

--- projects/cargill-button/src/lib/cargill-button.component.ts
+++ projects/cargill-button/src/lib/cargill-button.component.ts
@@ -1,1 +1,29 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
+@Component({
+  selector: 'cargill-button',
+  template: `
+    <button class="btn" [disabled]="disabled" (click)='onclick($event)'>{{name}}</button>
+  `,
+  styles: [`
+    .btn { background-color: #003641; color: white; outline: #003641; text-shadow: 2px 2px 5px black; font-size: 0.9rem; border-radius: 5px; padding: 0.275rem 0.5rem; }
+    .btn:hover { background-color: #268A95; color: white; outline: #268A95 }
+    .btn:focus { box-shadow: none!important }
+  `]
+})
+export class CargillButtonComponent implements OnInit {
+
+  @Input() disabled: boolean = false;
+  @Input() name!: any;
+  @Output() click = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  public onclick(event: Event) {
+    if (this.disabled) {
+      event.stopPropagation();
+    }
+  }
+}
---
+++ projects/cargill-button/src/lib/cargill-button.module.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CargillButtonComponent } from './cargill-button.component';
+
+@NgModule({
+  declarations: [
+    CargillButtonComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    CargillButtonComponent
+  ]
+})
+export class CargillButtonModule { }
