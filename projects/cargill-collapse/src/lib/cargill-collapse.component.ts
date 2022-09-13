86cd041dc00a5ca788e9047278ffb743bbe34a16
Santosh Kumar Hota <santosh.babul1990@gmail.com>
2022-09-13 14:39:13 +0530

    Add files via upload

--- projects/cargill-collapse/src/lib/cargill-collapse.component.ts
+++ projects/cargill-collapse/src/lib/cargill-collapse.component.ts
@@ -1,1 +1,33 @@
+import { Component, Input, OnInit } from '@angular/core';
 
+@Component({
+  selector: 'cargill-collapse',
+  template: `
+    <a class="btn mb-1" [style.backgroundColor]='bgColor' data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false"
+    aria-controls="collapseExample">
+    {{header}}
+    </a>
+    <br>
+    <div class="collapse" id="collapseExample">
+        <div class="card card-body">
+            {{message}}
+        </div>
+    </div>
+  `,
+  styles: [`
+    .btn { padding: 0.275rem 0.5rem }
+    .btn:hover { background-color: #268A95 !important; outline: #268A95 !important }
+    .btn:focus { box-shadow: none!important }
+  `]
+})
+export class CargillCollapseComponent implements OnInit {
+
+  @Input() header: any;
+  @Input() message: any;
+  @Input() bgColor: any = '#003641'
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+}
---
+++ projects/cargill-collapse/src/lib/cargill-collapse.module.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CargillCollapseComponent } from './cargill-collapse.component';
+
+@NgModule({
+  declarations: [
+    CargillCollapseComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    CargillCollapseComponent
+  ]
+})
+export class CargillCollapseModule { }
