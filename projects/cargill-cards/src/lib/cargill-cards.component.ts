610c1f06d7f95f207658f158e1da1491daf4aa07
Santosh Kumar Hota <santosh.babul1990@gmail.com>
2022-09-13 14:32:51 +0530

    Add files via upload

--- projects/cargill-cards/src/lib/cargill-cards.component.ts
+++ projects/cargill-cards/src/lib/cargill-cards.component.ts
@@ -1,1 +1,28 @@
+import { Component, Input, OnInit } from '@angular/core';
 
+@Component({
+  selector: 'cargill-cards',
+  template: `
+    <div class="card">
+      <img src={{imagePath}} alt="Card Image">
+      <div class="card-body mt-1" *ngIf="message !==''">
+          <p class="card-text">{{message}}</p>
+      </div>
+    </div>
+  `,
+  styles: [`
+    .card { border: none !important }
+    img { border-radius: 6px }
+    .card-body { border-radius: 6px; border: 1px solid rgba(0,0,0,.125) }
+  `]
+})
+export class CargillCardsComponent implements OnInit {
+
+  @Input() message: any = '';
+  @Input() imagePath: any;
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+}
---
+++ projects/cargill-cards/src/lib/cargill-cards.module.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CargillCardsComponent } from './cargill-cards.component';
+
+@NgModule({
+  declarations: [
+    CargillCardsComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    CargillCardsComponent
+  ]
+})
+export class CargillCardsModule { }
