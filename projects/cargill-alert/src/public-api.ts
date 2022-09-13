635f2b66cb6bd6786b4f2b194e799a4637fd53c4
Santosh Kumar Hota <santosh.babul1990@gmail.com>
2022-09-13 14:25:50 +0530

    Add files via upload

---
+++ projects/cargill-alert/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of cargill-alert
+ */
+
+export * from './lib/cargill-alert.service';
+export * from './lib/cargill-alert.component';
+export * from './lib/cargill-alert.module';
