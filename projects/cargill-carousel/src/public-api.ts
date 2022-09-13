250fb32b58c5ae73f74814103999e503cdd723b0
Santosh Kumar Hota <santosh.babul1990@gmail.com>
2022-09-13 14:35:46 +0530

    Add files via upload

---
+++ projects/cargill-carousel/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of cargill-carousel
+ */
+
+export * from './lib/cargill-carousel.service';
+export * from './lib/cargill-carousel.component';
+export * from './lib/cargill-carousel.module';
