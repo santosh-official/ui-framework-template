033f66220b9decb1c4f989884c71bfb168818487
Santosh Kumar Hota <santosh.babul1990@gmail.com>
2022-09-13 12:55:16 +0530

    Add files via upload

--- src/app/ui-common/services/common.service.ts
+++ src/app/ui-common/services/common.service.ts
@@ -1,1 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
+@Injectable({
+  providedIn: 'root'
+})
+
+export class CommonService {
+
+  constructor(private httpService: HttpClient) { }
+
+  public fetchTemplateInformation(): Observable<any> {
+    return this.httpService.get("./assets/data/template.json");
+  }
+
+  public fetchTableInformation(): Observable<any> {
+    return this.httpService.get("./assets/data/data.json");
+  }
+}
