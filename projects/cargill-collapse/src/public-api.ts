3be4069b72721b8a9496be906c8d7e335f8b237b
Santosh Kumar Hota <santosh.babul1990@gmail.com>
2022-09-13 14:39:57 +0530

    Add files via upload

---
+++ projects/cargill-collapse/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of cargill-collapse
+ */
+
+export * from './lib/cargill-collapse.service';
+export * from './lib/cargill-collapse.component';
+export * from './lib/cargill-collapse.module';
